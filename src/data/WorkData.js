77180cc0f44b211291bbf5ebb10ed330f8d8cc85
Mukhtorov-Asrorbek <asrorbekm245@gmail.com>
2022-03-26 18:51:37 +0500

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,59 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Travel and Tour Agency",
+    description:
+      "A travel booking website which allows user to join trips organized by team and get trip recommendations based on a survey.",
+    tags: ["javascript", "scss"],
+
+    demo: "https://travel-and-tour-agency.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/Travel-and-Tour-Agency",
+  },
+  {
+    id: 2,
+    name: "React Game",
+    description: "This game was on React js however without responsive",
+    tags: ["react", "scss"],
+
+    demo: "https://my-react-game.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/React-game",
+  },
+  {
+    id: 3,
+    name: "Drag and Drop",
+    description: "Beautiful and accessible drag and drop for lists with React",
+    tags: ["react", "scss"],
+
+    demo: "https://my-drag-drop.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/Drag-and-Drop",
+  },
+  {
+    id: 4,
+    name: "React Games",
+    description: "Responsive React Movies App With API",
+    tags: ["react", "styledComponents"],
+
+    demo: "https://asror-movies.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/Movies",
+  },
+  {
+    id: 5,
+    name: "Magic Match",
+    description: "a react memory game",
+    tags: ["javascript", "css"],
+
+    demo: "https://my-magic-match.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/Magic-Match",
+  },
+  {
+    id: 6,
+    name: "React Admin Panel App",
+    description: "React Admin Panel App without responsive",
+    tags: ["react", "scss"],
+
+    demo: "https://my-admin-panel.netlify.app/",
+    github: "https://github.com/Mukhtorov-Asrorbek/AdminPanel",
+  },
+];
